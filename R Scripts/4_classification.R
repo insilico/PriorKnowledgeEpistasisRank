5b4511bbb797cb3e0056e350124a953ba74ea1a4
Saeid Parvandeh <saeid@saeids-mbp.ad.bcm.edu>
2018-09-17 09:43:20 -0500

    document()

--- R Scripts/4_classification.R
+++ R Scripts/4_classification.R
@@ -56,7 +56,7 @@ for (i in 1:5){
     top_camb_page_woPK <- camb_page_woPK.sort[1:200]
     page_woPK_inner.features <- c(page_woPK_inner.features, top_camb_page_woPK)
     page_woPK_features <- c(page_woPK_features, top_camb_page_woPK)
-
+    
   }
   camb_page_woPK.expr <- camb_fltr.expr[, unique(as.character(names(page_woPK_inner.features)))] 
   # Training
@@ -74,10 +74,8 @@ for (i in 1:5){
   page_woPK_test_acc <- c(page_woPK_test_acc, accuracy(jap_class, jap_page_woPK_bst.pred)$prop.correct)
 }
 save(page_woPK_features, file = "page_woPK_features.RData")
-cat("Training accuracy")
-page_woPK_train_acc[which.min(page_woPK_train_acc - page_woPK_test_acc)]
-cat("Testing accuracy")
-page_woPK_test_acc[which.min(page_woPK_train_acc - page_woPK_test_acc)]
+pr_wo_train <- page_woPK_train_acc[which.min(page_woPK_train_acc - page_woPK_test_acc)]
+pr_wo_test <- page_woPK_test_acc[which.min(page_woPK_train_acc - page_woPK_test_acc)]
 
 #------------- nestedCV feature selection-Katz -------------#
 katz_woPK_features <- NULL
@@ -123,10 +121,8 @@ for (i in 1:5){
   katz_woPK_test_acc <- c(katz_woPK_test_acc, accuracy(jap_class, jap_katz_woPK_bst.pred)$prop.correct)
 }
 save(katz_woPK_features, file = "katz_woPK_features.RData")
-cat("Training accuracy")
-katz_woPK_train_acc[which.min(katz_woPK_train_acc - katz_woPK_test_acc)]
-cat("Testing accuracy")
-katz_woPK_test_acc[which.min(katz_woPK_train_acc - katz_woPK_test_acc)]
+k_wo_train <- katz_woPK_train_acc[which.min(katz_woPK_train_acc - katz_woPK_test_acc)]
+k_wo_test <- katz_woPK_test_acc[which.min(katz_woPK_train_acc - katz_woPK_test_acc)]
 
 #---------- nestedCV feature selection-EpistasisKatz -----------#
 ekatz_woPK_features <- NULL
@@ -174,10 +170,8 @@ for (i in 1:5){
   ekatz_woPK_test_acc <- c(ekatz_woPK_test_acc, accuracy(jap_class, jap_ekatz_woPK_bst.pred)$prop.correct)
 }
 save(ekatz_woPK_features, file = "ekatz_woPK_features.RData")
-cat("Training accuracy")
-ekatz_woPK_train_acc[which.min(ekatz_woPK_train_acc - ekatz_woPK_test_acc)]
-cat("Testing accuracy")
-ekatz_woPK_test_acc[which.min(ekatz_woPK_train_acc - ekatz_woPK_test_acc)]
+ek_wo_train <- ekatz_woPK_train_acc[which.min(ekatz_woPK_train_acc - ekatz_woPK_test_acc)]
+ek_wo_test <- ekatz_woPK_test_acc[which.min(ekatz_woPK_train_acc - ekatz_woPK_test_acc)]
 
 
 #------------ nestedCV feature selection-EpistasisRank  ------------#
@@ -221,10 +215,8 @@ for (i in 1:5){
   ER_woPK_test_acc <- c(ER_woPK_test_acc, accuracy(jap_class, jap_ER_woPK_bst.pred)$prop.correct)
 }
 save(ER_woPK_features, file = "ER_woPK_features.RData")
-cat("Training accuracy")
-ER_woPK_train_acc[which.min(ER_woPK_train_acc - ER_woPK_test_acc)]
-cat("Testing accuracy")
-ER_woPK_test_acc[which.min(ER_woPK_train_acc - ER_woPK_test_acc)]
+er_wo_train <- ER_woPK_train_acc[which.min(ER_woPK_train_acc - ER_woPK_test_acc)]
+er_wo_test <- ER_woPK_test_acc[which.min(ER_woPK_train_acc - ER_woPK_test_acc)]
 
 
 #--------- nestedCV feature selection-coefficient of variantion ---------#
@@ -235,7 +227,7 @@ cov_test_acc <- NULL
 outer_folds <- caret::createFolds(camb_class, 5, list = FALSE)
 # create inner folds
 for (i in 1:5){
-  cov_woPK_inner.features <- NULL
+  cov_inner.features <- NULL
   inner_folds <- caret::createFolds(camb_class[outer_folds!=i], 5, list = TRUE)
   for (j in 1:length(inner_folds)){
     fold_idx <- which(outer_folds != i)[-inner_folds[[j]]] 
@@ -245,8 +237,9 @@ for (i in 1:5){
     camb_expr.fltr_cov <- camb_fltr.expr
     while (ncol(camb_expr.fltr_cov)>200){
       camb_expr.fltr_cov <- t(cov.filter(exprData,threshold = thresh)$fdata)
-      thresh <- thresh-0.001
+      thresh <- thresh-0.00001
     }
+    dim(camb_expr.fltr_cov)
     cov_inner.features <- c(cov_inner.features, colnames(camb_expr.fltr_cov))
     cov_features <- c(cov_features, colnames(camb_expr.fltr_cov))
   }
@@ -265,11 +258,9 @@ for (i in 1:5){
   jap_cov_bst.pred <- predict(camb_cov_bst.model, as.matrix(jap_cov.expr))
   cov_test_acc <- c(cov_test_acc, accuracy(jap_class, jap_cov_bst.pred)$prop.correct)
 }
-save(cov_features, file = "cv_features.RData")
-cat("Training accuracy")
-cov_train_acc[which.min(cov_train_acc - cov_train_acc)]
-cat("Testing accuracy")
-cov_train_acc[which.min(cov_train_acc - cov_train_acc)]
+save(cov_features, file = "cov_features.RData")
+cov_train <- cov_train_acc[which.min(cov_train_acc - cov_test_acc)]
+cov_test <- cov_test_acc[which.min(cov_train_acc - cov_test_acc)]
 
 
 ######################################
@@ -322,10 +313,8 @@ for (i in 1:5){
   page_wPK_test_acc <- c(page_wPK_test_acc, accuracy(jap_class, jap_page_wPK_bst.pred)$prop.correct)
 }
 save(page_wPK_features, file = "page_wPK_features.RData")
-cat("Training accuracy")
-page_wPK_train_acc[which.min(page_wPK_train_acc - page_wPK_test_acc)]
-cat("Testing accuracy")
-page_wPK_test_acc[which.min(page_wPK_train_acc - page_wPK_test_acc)]
+pr_w_train <- page_wPK_train_acc[which.min(page_wPK_train_acc - page_wPK_test_acc)]
+pr_w_test <- page_wPK_test_acc[which.min(page_wPK_train_acc - page_wPK_test_acc)]
 
 
 #---------- nestedCV feature selection-Katz -----------#
@@ -371,10 +360,8 @@ for (i in 1:5){
   katz_wPK_test_acc <- c(katz_wPK_test_acc, accuracy(jap_class, jap_katz_wPK_bst.pred)$prop.correct)
 }
 save(katz_wPK_features, file = "katz_wPK_features.RData")
-cat("Training accuracy")
-katz_wPK_train_acc[which.min(katz_wPK_train_acc - katz_wPK_test_acc)]
-cat("Testing accuracy")
-katz_wPK_test_acc[which.min(katz_wPK_train_acc - katz_wPK_test_acc)]
+k_w_train <- katz_wPK_train_acc[which.min(katz_wPK_train_acc - katz_wPK_test_acc)]
+k_w_test <- katz_wPK_test_acc[which.min(katz_wPK_train_acc - katz_wPK_test_acc)]
 
 
 #--------- nestedCV feature selection-EpistasisKatz ---------#
@@ -423,10 +410,8 @@ for (i in 1:5){
   ekatz_wPK_test_acc <- c(ekatz_wPK_test_acc, accuracy(jap_class, jap_ekatz_wPK_bst.pred)$prop.correct)
 }
 save(ekatz_wPK_features, file = "ekatz_wPK_features.RData")
-cat("Training accuracy")
-ekatz_wPK_train_acc[which.min(ekatz_wPK_train_acc - ekatz_wPK_test_acc)]
-cat("Testing accuracy")
-ekatz_wPK_test_acc[which.min(ekatz_wPK_train_acc - ekatz_wPK_test_acc)]
+ek_w_train <- ekatz_wPK_train_acc[which.min(ekatz_wPK_train_acc - ekatz_wPK_test_acc)]
+ek_w_test <- ekatz_wPK_test_acc[which.min(ekatz_wPK_train_acc - ekatz_wPK_test_acc)]
 
 
 #---------- nestedCV feature selection-EpistasisRank ---------#
@@ -470,43 +455,138 @@ for (i in 1:5){
   ER_wPK_test_acc <- c(ER_wPK_test_acc, accuracy(jap_class, jap_ER_wPK_bst.pred)$prop.correct)
 }
 save(ER_wPK_features, file = "ER_wPK_features.RData")
-cat("Training accuracy")
-ER_wPK_train_acc[which.min(ER_wPK_train_acc - ER_wPK_test_acc)]
-cat("Testing accuracy")
-ER_wPK_test_acc[which.min(ER_wPK_train_acc - ER_wPK_test_acc)]
+er_w_train <- ER_wPK_train_acc[which.min(ER_wPK_train_acc - ER_wPK_test_acc)]
+er_w_test <- ER_wPK_test_acc[which.min(ER_wPK_train_acc - ER_wPK_test_acc)]
+
+#--------- nestedCV feature selection-coefficient of variantion ---------#
+cov_features <- NULL
+cov_train_acc <- NULL
+cov_test_acc <- NULL
+# create outer folds
+outer_folds <- caret::createFolds(camb_class, 5, list = FALSE)
+# create inner folds
+for (i in 1:5){
+  cov_woPK_inner.features <- NULL
+  inner_folds <- caret::createFolds(camb_class[outer_folds!=i], 5, list = TRUE)
+  for (j in 1:length(inner_folds)){
+    fold_idx <- which(outer_folds != i)[-inner_folds[[j]]] 
+    # CV filtering
+    exprData <- t(camb_fltr.expr[fold_idx, ])
+    thresh <- .5
+    camb_expr.fltr_cov <- camb_fltr.expr
+    while (ncol(camb_expr.fltr_cov)>200){
+      camb_expr.fltr_cov <- t(cov.filter(exprData,threshold = thresh)$fdata)
+      thresh <- thresh-0.001
+    }
+    cov_inner.features <- c(cov_inner.features, colnames(camb_expr.fltr_cov))
+    cov_features <- c(cov_features, colnames(camb_expr.fltr_cov))
+  }
+  camb_cov.expr <- camb_fltr.expr[, unique(as.character(cov_inner.features))] 
+  # Training
+  camb_dtrain <- xgb.DMatrix(as.matrix(camb_cov.expr), label = camb_class)
+  camb_cov_bst.model <- xgb.train(data=camb_dtrain, max.depth=1, eta = .01, nthread = 2, 
+                                  nround=1, gamma = 0, min_child_weight = 1, max_delta_step = 1, 
+                                  subsample = 0.5, objective = "binary:logistic")
+  
+  camb_cov_bst.pred <- predict(camb_cov_bst.model, as.matrix(camb_cov.expr))
+  cov_train_acc <- c(cov_train_acc, accuracy(camb_class, camb_cov_bst.pred)$prop.correct)
+  
+  # Testing
+  jap_cov.expr <- jap_fltr.expr[, unique(as.character(cov_inner.features))]
+  jap_cov_bst.pred <- predict(camb_cov_bst.model, as.matrix(jap_cov.expr))
+  cov_test_acc <- c(cov_test_acc, accuracy(jap_class, jap_cov_bst.pred)$prop.correct)
+}
+save(cov_features, file = "cv_features.RData")
+cov_train <- cov_train_acc[which.min(cov_train_acc - cov_test_acc)]
+cov_test <- cov_test_acc[which.min(cov_train_acc - cov_test_acc)]
+
+
+#--------- privateEC classificaton ---------#
+camb_data <- data.frame(camb_fltr.expr, class = as.factor(camb_class))
+jap_data <- data.frame(jap_fltr.expr, class = as.factor(jap_class))
+
+train.splits <- splitDataset(camb_data, pct.train = 0.5, pct.holdout = 0.5, label = "class")
+train.splits$train[, "class"] <- as.factor(ifelse(train.splits$train[, "class"] == -1, 1, 0))
+train.splits$holdout[, "class"] <- as.factor(ifelse(train.splits$holdout[, "class"] == -1, 1, 0))
+pec_results <- privateEC(train.ds = train.splits$train, holdout.ds = train.splits$holdout, validation.ds = jap_data,
+                         label = "class", method.model = "classification", is.simulated = FALSE, tau.param = 100,
+                         update.freq = 50, rf.ntree = 500, verbose = FALSE)
+
+multiple.max.indices <- which(pec_results$algo.acc$holdout.acc==max(pec_results$algo.acc$holdout.acc))
+last.max <- multiple.max.indices[length(multiple.max.indices)]
+pec_train <- pec_results$algo.acc$holdout.acc[last.max]
+pec_hold <- pec_results$algo.acc$validation.acc[last.max]
+
+
+#--------- xgboost only classification ---------#
+# Training
+camb_dtrain <- xgb.DMatrix(as.matrix(camb_fltr.expr), label = camb_class)
+camb_xgb_model <- xgb.train(data=camb_dtrain, max.depth=4, eta = .3, nthread = 2, 
+                            nround=2, gamma = 0, min_child_weight = 1, max_delta_step = 1, 
+                            subsample = 1, objective = "binary:logistic")
+
+camb_xgb_pred <- predict(camb_xgb_model, as.matrix(camb_fltr.expr))
+xgb_train <- accuracy(camb_class, camb_xgb_pred)$prop.correct
+
+# Testing
+jap_xgb_pred <- predict(camb_xgb_model, as.matrix(jap_fltr.expr))
+xgb_test <- accuracy(jap_class, jap_xgb_pred)$prop.correct
 
 
 ###############################
 #---- Classification plot ----#
 ###############################
-PR.Katz_df <- data.frame(matrix(0, 8, 4))
-PR.Katz_df$Type <- factor(c("woPK", "wPK", "woPK", "wPK", "woPK", "wPK", "woPK", "wPK"), levels = c("woPK", "wPK"))
-PR.Katz_df$Method <- factor(c("PageRank", "PageRank", "Katz", "Katz", "PageRank", "PageRank", "Katz", "Katz"), 
-                            levels = c("PageRank", "Katz"))
-PR.Katz_df$Accuracy <- c(0.85, 0.87, 0.92, 0.90, 0.75, 0.87, 0.81, 0.87) 
-PR.Katz_df$Status <- c("Cambridge-Train", "Cambridge-Train", "Cambridge-Train", "Cambridge-Train", 
-                       "Japan-Test", "Japan-Test", "Japan-Test", "Japan-Test")
+PR.Katz_df <- data.frame(
+  Data = factor(c("Cambridge-Train", "Japan-Test", "Cambridge-Train", "Japan-Test", 
+                  "Cambridge-Train", "Japan-Test", "Cambridge-Train", "Japan-Test"), levels = c("Cambridge-Train", 
+                                                                                                "Japan-Test")),
+  Method = factor(c("PR", "PR", "Katz", "Katz", "PR", "PR", "Katz", "Katz"), 
+                  levels = c("PR", "Katz")),
+  Accuracy = c(pr_wo_train, pr_wo_test, k_wo_train, k_wo_test, pr_w_train, pr_w_test, k_w_train, k_w_test),
+  Status  = c( "no Prior Knowledge", "no Prior Knowledge", "no Prior Knowledge", "no Prior Knowledge", 
+               "Prior Knowledge", "Prior Knowledge", "Prior Knowledge", "Prior Knowledge")
+)
 
-EK.ER_df <- data.frame(matrix(0, 8, 4))
-EK.ER_df$Type <- factor(c("woPK", "wPK", "woPK", "wPK", "woPK", "wPK", "woPK", "wPK"), levels = c("woPK", "wPK"))
-EK.ER_df$Method <- factor(c("Epistasis-Katz", "Epistasis-Katz", "EpistasisRank", "EpistasisRank", 
-                       "Epistasis-Katz", "Epistasis-Katz", "EpistasisRank", "EpistasisRank"), 
-                       levels = c("Epistasis-Katz", "EpistasisRank"))
-EK.ER_df$Accuracy <- c(0.85, 0.87, 0.85, 0.82, 0.68, 0.87, 0.75, 0.81)  
-EK.ER_df$Status <- c("Cambridge-Train", "Cambridge-Train", "Cambridge-Train", "Cambridge-Train", 
-                       "Japan-Test", "Japan-Test", "Japan-Test", "Japan-Test")
+EK.ER_df <- data.frame(
+  Data = factor(c("Cambridge-Train", "Japan-Test", "Cambridge-Train", "Japan-Test", 
+                  "Cambridge-Train", "Japan-Test", "Cambridge-Train", "Japan-Test"), levels = c("Cambridge-Train", 
+                                                                                                "Japan-Test")),
+  Method = factor(c("EK", "EK", "ER", "ER", 
+                    "EK", "EK", "ER", "ER"), 
+                  levels = c("EK", "ER")),
+  Accuracy = c(ek_wo_train, ek_wo_test, er_wo_train, er_wo_test, ek_w_train, ek_w_test, er_w_train, er_w_test),
+  Status = c("no Prior Knowledge", "no Prior Knowledge", "no Prior Knowledge", "no Prior Knowledge", 
+             "Prior Knowledge", "Prior Knowledge", "Prior Knowledge", "Prior Knowledge")
+)
+
+cov_pec_df <- data.frame(
+  Data = factor(c("Cambridge-Train", "Cambridge-Train", "Japan-Test", "Japan-Test")),
+  Method = factor(c("CoV", "xgb", "CoV", "xgb"), levels = c("CoV", "xgb")),
+  Accuracy = c(cov_train, xgb_train, cov_test, xgb_test),
+  Status = c("no Prior Knowledge", "no Prior Knowledge", "no Prior Knowledge", "no Prior Knowledge")
+)
 
 
-library(ggplot2)
-p1 <- ggplot(PR.Katz_df, aes(x = Type, y = Accuracy, group = Method)) +
+p1 <- ggplot(PR.Katz_df, aes(x = Data, y = Accuracy, group = Method)) +
+  ggtitle("Co-expression Networks") + theme(plot.title = element_text(hjust = 0.5)) +
   geom_point() + geom_line(aes(linetype=Method)) +
+  geom_point(data = cov_pec_df, mapping = aes(x = Data, y = Accuracy, shape = Method)) +
+  theme(axis.title.x=element_blank()) +
+  scale_shape_manual(values=c(6,5)) +
   scale_linetype_manual(values=c("twodash", "solid"))+
   facet_wrap(~Status) +
-  theme(legend.position="top")
-p2 <- ggplot(EK.ER_df, aes(x = Type, y = Accuracy, group = Method)) +
+  theme(legend.position=c(0.95, 0.5), legend.text=element_text(size=6), legend.title = element_text(size=6)) +
+  guides(group = guide_legend(order=1),
+         shape = guide_legend(order=2))
+p2 <- ggplot(EK.ER_df, aes(x = Data, y = Accuracy, group = Method)) +
+  ggtitle("Epistasis Networks") + theme(plot.title = element_text(hjust = 0.5)) +
   geom_point() + geom_line(aes(linetype=Method)) +
+  geom_point(data = cov_pec_df, mapping = aes(x = Data, y = Accuracy, shape = Method)) +
+  theme(axis.title.x=element_blank()) +
+  scale_shape_manual(values=c(6,5)) +
   scale_linetype_manual(values=c("twodash", "solid"))+
   facet_wrap(~Status) +
-  theme(legend.position="top")
-library(gridExtra)
+  theme(legend.position=c(0.95, 0.5), legend.text=element_text(size=6), legend.title = element_text(size=6)) +
+  guides(group = guide_legend(order=1),
+         shape = guide_legend(order=2))
 grid.arrange(p1, p2, nrow = 2)
--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # PriorKnowledgeEpistasisRank
 Epistasis network centralities that incorporate prior knowledge
+Author: Saeid Parvandeh and Brett McKinney
 
-![Pathway plots](Pathway_sub_plot.png)
+### How to install required packages
+    > install.packages("gdata")
+    > library(gdata)
+    > install.packages("devtools", repos="http://cran.r-project.org")
+    > require(devtools)
+    > install_github("insilico/privateEC")
+    > library(privateEC)
+    > install.packages("lattice")
+    > library(lattice)
+    > install.packages("caret", repos="http://cran.r-project.org")
+    > library(caret)
+    > install.packages("glmnet", repos="http://cran.r-project.org")
+    > library(glmnet)
+    > install.packages("CORElearn", repos="http://cran.r-project.org")
+    > library(CORElearn)
+    > install.packages("xgboost", repos="http://cran.r-project.org")
+    > library(xgboost)
 
-![Accuracy plots](Acc_plot.png)
+### How to find vignettes
+You will be able to find two vignettes, pathway.Rmd and classification.Rmd. The first one manifests the steps of creating pathaway enrichment and how prior knowledge improve Reactome Pathways for MDD, and the second shows the classification accuracies where we add prior knowledge. 
+
+### Accuracy plot
+Prior knowledge effect of epistasis centrality on training accuracy (Cambridge data on left) and independent validation accuracy (Japan data on right). Accuracies of four different centrality-based feature selection methods with prior knowledge (PK) and without prior knowledge (no PK) using nested cross-validated classification with xgboosted trees. Centralities include PageRank (PR), Katz, Epistasis Katz (EK), EpistasisRank (ER). We compare the accuracy with no prior knowledge and coefficient of variation (CoV) filtering (triangles) and xgboost only on all genes (diamonds).
+
+![Accuracy plots](Acc_original_plot_revised.png)
